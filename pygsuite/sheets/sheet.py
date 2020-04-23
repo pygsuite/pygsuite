717637cc2c82bbd7707b4595adfcc7b33c18aa0d
Sam Gaudet <sgaudet1@babson.edu>
2020-04-23 10:35:48 -0400

    add data insert / read methods

--- pygsuite/sheets/sheet.py
+++ pygsuite/sheets/sheet.py
@@ -1,25 +1,35 @@
+import pandas as pd
+
 from .worksheet import Worksheet
 
 
 class Spreadsheet:
+
+    # ValueInputOption objects: https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+    VALUE_INPUT_OPTIONS = ["RAW", "USER_ENTERED"]
+    # Dimensions objects: https://developers.google.com/sheets/api/reference/rest/v4/Dimension
+    DIMENSIONS = ["ROWS", "COLUMNS"]
+
     def __init__(self, service, id):
         self.service = service
         self.id = id
         self._sheet = service.open_by_key(id)
-        self._change_queue = []
+        self._batchUpdate_queue = []
+        # self._batchGet_queue = []
 
     def id(self):
         return self._sheet["id"]
 
     def flush(self):
-        out = (
+
+        update_response = (
             self.service.spreadsheets()
-            .batchUpdate(body={"requests": self._change_queue}, spreadsheetId=self.id)
+            .batchUpdate(body={"requests": self._batchUpdate_queue}, spreadsheetId=self.id)
             .execute()["replies"]
         )
-        self._change_queue = []
+        self._batchUpdate_queue = []
         self.refresh()
-        return out
+        return update_response
 
     @property
     def worksheets(self):
@@ -30,3 +40,39 @@ class Spreadsheet:
 
     def refresh(self):
         self._sheet = self.service.open_by_key(self.id)
+
+    def get_data_from_ranges(self, ranges):
+
+        get_response = self._client.spreadsheets().values().batchGet(
+            spreadsheetId=self._spreadsheetId, ranges=ranges).execute()
+
+        dfs = dict()
+
+        for valueRange in get_response.get("valueRanges"):
+
+            values = valueRange.get("values")
+            df = pd.DataFrame.from_records(data=values)
+            dfs[valueRange.get("range")] = df
+
+        return dfs
+
+    def insert_data_from_df(self, df, range, valueInputOption="RAW", majorDimension="ROWS"):
+
+        # TODO: change to helpful exceptions
+        assert valueInputOption in self.VALUE_INPUT_OPTIONS
+        assert majorDimension in self.DIMENSIONS
+
+        data = df.values.tolist()
+
+        valueRange = {
+            "range": range,
+            "majorDimension": majorDimension,
+            "values": data
+        }
+
+        request = {
+            "valueInputOption": valueInputOption,
+            "data": [valueRange]
+        }
+
+        self._batchUpdate_queue.append(request)
