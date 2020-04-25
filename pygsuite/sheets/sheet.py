9cbaa001ddd61d6799b0f7cdc1ff959819ede92f
Sam Gaudet <sgaudet1@babson.edu>
2020-04-24 21:56:31 -0400

    lots of updates with data methods

--- pygsuite/sheets/sheet.py
+++ pygsuite/sheets/sheet.py
@@ -1,3 +1,4 @@
+from googleapiclient.errors import HttpError
 import pandas as pd
 
 from pygsuite.sheets.worksheet import Worksheet
@@ -10,51 +11,78 @@ class Spreadsheet:
     VALUE_INPUT_OPTIONS = ["RAW", "USER_ENTERED"]
     # Dimensions objects: https://developers.google.com/sheets/api/reference/rest/v4/Dimension
     DIMENSIONS = ["ROWS", "COLUMNS"]
+    # ValueRenderOption objects: https://developers.google.com/sheets/api/reference/rest/v4/ValueRenderOption
+    VALUE_RENDER_OPTIONS = ["FORMATTED_VALUE", "UNFORMATTED_VALUE", "FORMULA"]
+    # DateTimeRenderOption objects: https://developers.google.com/sheets/api/reference/rest/v4/DateTimeRenderOption
+    DATE_TIME_RENDER_OPTIONS = ["SERIAL_NUMBER", "FORMATTED_STRING"]
 
     def __init__(self, service, id):
         self.service = service
         self.id = id
-        self._sheet = service.open_by_key(id)
-        self._batchUpdate_queue = []
+        self._spreadsheet = self.service.spreadsheets().get(spreadsheetId=id).execute()
+        self._properties = self._spreadsheet.get("properties")
 
-    def id(self):
-        return self._sheet["id"]
+        # queues to add to and run in flush()
+        self._spreadsheets_batchUpdate_queue = []
+        self._values_batchUpdate_queue = []
+        self._values_batchGet_queue = []
+
+    def __getitem__(self, key):
+
+        # if the key is an integer, use the int as worksheet index
+        if isinstance(key, int):
+            return self.worksheets[key]
+        # if the key is a string, try to match to a worksheet name
+        elif isinstance(key, str):
+            try:
+                return [worksheet for worksheet in self.worksheets if worksheet.name == key][0]
+            except ValueError:
+                raise ValueError("No worksheet with the title '{key}' exists. The worksheets included in your spreadsheet are: {worksheets}".format(key=key, worksheets=[worksheet.name for worksheet in self.worksheets]))
+        else:
+            raise ValueError("Please enter the sheet index or name you are trying to get.")
+
+    @property
+    def worksheets(self):
+        return [Worksheet(sheet, self) for sheet in self._spreadsheet.get("sheets")]
 
     @retry((HttpError), tries=3, delay=10, backoff=5)
     def flush(self, reverse=False):
 
         if reverse:
-            base = reversed(self._batchUpdate_queue)
+            base = reversed(self._spreadsheets_batchUpdate_queue)
         else:
-            base = self._batchUpdate_queue
+            base = self._spreadsheets_batchUpdate_queue
 
         update_response = (
-            self.service.spreadsheets()
+            self.service
+            .spreadsheets()
             .batchUpdate(body={"requests": base}, spreadsheetId=self.id)
-            .execute()["replies"]
+            .execute()["responses"]
         )
-        self._batchUpdate_queue = []
+
+        self._spreadsheets_batchUpdate_queue = []
+        self._values_batchUpdate_queue = []
+        self._values_batchGet_queue = []
         self.refresh()
         return update_response
 
-    @property
-    def worksheets(self):
-        return [Worksheet(item, self._sheet) for item in self._sheet.worksheets()]
-
-    def __getitem__(self, item):
-        return self.worksheets[item]
-
     def refresh(self):
-        self._sheet = self.service.open_by_key(self.id)
+        self._spreadsheet = self.service.spreadsheets().get(spreadsheetId=self.id).execute()
 
     def get_data_from_ranges(self, ranges):
 
-        get_response = self._client.spreadsheets().values().batchGet(
-            spreadsheetId=self.id, ranges=ranges).execute()
+        get_response = (
+            self.service.spreadsheets()
+            .values()
+            .batchGet(spreadsheetId=self.id, ranges=ranges)
+            .execute()
+        )
 
         dfs = dict()
 
         for valueRange in get_response.get("valueRanges"):
+
+            # TODO: do we want to handle header rows here, or leave that to end users?
 
             values = valueRange.get("values")
             df = pd.DataFrame.from_records(data=values)
@@ -62,25 +90,47 @@ class Spreadsheet:
 
         return dfs
 
-    def insert_data_from_df(self, df, range, valueInputOption="RAW", majorDimension="ROWS"):
+    def insert_data_from_df(
+        self,
+        df,
+        range,
+        valueInputOption="RAW",
+        majorDimension="ROWS",
+        responseValueRenderOption="FORMATTED_VALUE",
+        responseDateTimeRenderOption="SERIAL_NUMBER",
+    ):
 
         # TODO: change to helpful exceptions
+
         assert valueInputOption in self.VALUE_INPUT_OPTIONS
         assert majorDimension in self.DIMENSIONS
+        assert responseValueRenderOption in self.VALUE_RENDER_OPTIONS
+        assert responseDateTimeRenderOption in self.DATE_TIME_RENDER_OPTIONS
 
+        header = df.columns.values.tolist()
         data = df.values.tolist()
 
-        valueRange = {
-            "range": range,
-            "majorDimension": majorDimension,
-            "values": data
-        }
+        values = []
+        if len(header) > 0:
+            values.append(header)
+        for row in data:
+            values.append(row)
+
+        valueRange = {"range": range, "majorDimension": majorDimension, "values": values}
 
         request = {
             "valueInputOption": valueInputOption,
-            "data": [valueRange]
+            "data": [valueRange],
+            "includeValuesInResponse": False,
+            "responseValueRenderOption": responseValueRenderOption,
+            "responseDateTimeRenderOption": responseDateTimeRenderOption,
         }
 
-        self._batchUpdate_queue.append(request)
+        response = (
+            self.service.spreadsheets()
+            .values()
+            .batchUpdate(spreadsheetId=self.id, body=request)
+            .execute()
+        )
 
-    def
+        return response
--- pygsuite/sheets/worksheet.py
+++ pygsuite/sheets/worksheet.py
@@ -15,14 +15,22 @@ def index_to_alphabet(idx):
 
 
 class Worksheet(object):
-    def __init__(self, element, sheet):
-        self._worksheet = element
-        self._sheet = sheet
-        self._properties = self._worksheet._properties
+
+    def __init__(self, worksheet, spreadsheet):
+        self._worksheet = worksheet
+        self._spreadsheet = spreadsheet
+        self._properties = self._worksheet["properties"]
+
+    def __getitem__(self, cell_range):
+        return self.values_from_range(cell_range)
 
     @property
     def name(self):
         return self._properties["title"]
+
+    @property
+    def id(self):
+        return self._properties["sheetId"]
 
     @property
     def row_count(self):
@@ -32,35 +40,42 @@ class Worksheet(object):
     def column_count(self):
         return self._properties["gridProperties"]["columnCount"]
 
-    @property
-    def values(self):
-        return self._sheet.values_get(self.name)
-        # out = []
-        # for idx in range(1,self.row_count):
-        #     out.append(self._worksheet.row_values(idx))
-        # return out
+    # @property
+    # def values(self):
+    #     return self._spreadsheet.values_get(self.name)
 
-    def values_range(self, startcol, startrow, endcol, endrow):
+    def range_from_indexes(self, startcol, startrow, endcol, endrow):
+
         assert startcol <= endcol
         assert startrow <= endrow
+
         start_index = f"{index_to_alphabet(startcol)}{startrow}"
         end_index = f"{index_to_alphabet(endcol)}{endrow}"
         range_label = f"{self.name}!{start_index}:{end_index}"
-        return self._sheet.values_get(range_label)
+
+        return range_label
+
+    def values_from_range(self, cell_range):
+
+        worksheet_range = f"{self.name}!{cell_range}"
+        df_dict = self._spreadsheet.get_data_from_ranges(worksheet_range)
+        return df_dict[worksheet_range]
 
     @property
     def all_values(self):
-        output = self.values_range(1, 1, self.column_count, self.row_count)
-        return output["values"]
 
-    @property
-    def dataframe(self):
-        values = self.all_values
-        header = values[0]
-        processed = []
-        for row in values[1:]:
-            if len(row) < len(header):
-                diff = len(header) - len(row)
-                row += [""] * diff
-            processed.append(row)
-        return pd.DataFrame.from_records(processed, columns=header)
+        worksheet_range = self.range_from_indexes(1, 1, self.column_count, self.row_count)
+        values = self._spreadsheet.get_data_from_ranges(worksheet_range)
+        return values
+
+    # @property
+    # def dataframe(self):
+    #     values = self.all_values
+    #     header = values[0]
+    #     processed = []
+    #     for row in values[1:]:
+    #         if len(row) < len(header):
+    #             diff = len(header) - len(row)
+    #             row += [""] * diff
+    #         processed.append(row)
+    #     return pd.DataFrame.from_records(processed, columns=header)
