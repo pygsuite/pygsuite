880d2d8c5b0a1eac09c680a4f19b34020ff950f5
Sam Gaudet <sgaudet1@babson.edu>
2020-05-19 20:47:57 -0400

    add docstrings and some formatting cleanup

--- pygsuite/sheets/sheet.py
+++ pygsuite/sheets/sheet.py
@@ -1,3 +1,7 @@
+from enum import Enum
+from typing import Optional, Union
+
+from googleapiclient.discovery import Resource
 from googleapiclient.errors import HttpError
 import pandas as pd
 
@@ -5,15 +9,56 @@ from pygsuite.sheets.worksheet import Worksheet
 from pygsuite.utility.decorators import retry
 
 
-def create_new_spreadsheet(service, title):
+class ValueInputOption(Enum):
+    """ValueInputOption: Determines how data should be interpreted.
+
+    Google docs: https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+    """
+
+    RAW = "RAW"
+    USER_ENTERED = "USER_ENTERED"
+
+
+class Dimension(Enum):
+    """Dimension: Indicates which dimension an operation should apply to.
+
+    Google docs: https://developers.google.com/sheets/api/reference/rest/v4/Dimension
+    """
+
+    ROWS = "ROWS"
+    COLUMNS = "COLUMNS"
+
+
+class ValueRenderOption(Enum):
+    """ValueRenderOption: Determines how values should be rendered in the output.
+
+    Google docs: https://developers.google.com/sheets/api/reference/rest/v4/ValueRenderOption
+    """
+
+    FORMATTED_VALUE = "FORMATTED_VALUE"
+    UNFORMATTED_VALUE = "UNFORMATTED_VALUE"
+    FORMULA = "FORMULA"
+
+
+class DateTimeRenderOption(Enum):
+    """DateTimeRenderOption: Determines how dates should be rendered in the output.
+
+    Google docs: https://developers.google.com/sheets/api/reference/rest/v4/DateTimeRenderOption
+    """
+
+    SERIAL_NUMBER = "SERIAL_NUMBER"
+    FORMATTED_STRING = "FORMATTED_STRING"
+
+
+def create_new_spreadsheet(service: Resource, title: str):
     """Function to create a new spreadsheet given a client connection to the GSuite API,
        and a title for the new sheet.
 
     # TODO: add functionality to specify "filepath"?
 
     Args:
-        service (googleapiclient.discovery.Resource): connection to the Google API Sheets resource.
-        title (str): name for the new spreadsheet document
+        service (googleapiclient.discovery.Resource): Connection to the Google API Sheets resource.
+        title (str): Name for the new spreadsheet document
 
     Returns:
         id (str): the id of the spreadsheet
@@ -35,17 +80,8 @@ class Spreadsheet:
     """Base class for the GSuite Spreadsheets API.
     """
 
-    # ValueInputOption objects: https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
-    VALUE_INPUT_OPTIONS = ["RAW", "USER_ENTERED"]
-    # Dimensions objects: https://developers.google.com/sheets/api/reference/rest/v4/Dimension
-    DIMENSIONS = ["ROWS", "COLUMNS"]
-    # ValueRenderOption objects: https://developers.google.com/sheets/api/reference/rest/v4/ValueRenderOption
-    VALUE_RENDER_OPTIONS = ["FORMATTED_VALUE", "UNFORMATTED_VALUE", "FORMULA"]
-    # DateTimeRenderOption objects: https://developers.google.com/sheets/api/reference/rest/v4/DateTimeRenderOption
-    DATE_TIME_RENDER_OPTIONS = ["SERIAL_NUMBER", "FORMATTED_STRING"]
-
     def __init__(
-        self, id, client=None,
+        self, id: str, client: Optional[Resource] = None,
     ):
         """Method to initialize the class.
 
@@ -53,9 +89,9 @@ class Spreadsheet:
         of the spreadsheet and create a _spreadsheet object to execute other requests of the API.
 
         Args:
-            service (googleapiclient.discovery.Resource): connection to the Google API Sheets resource.
             id (str): id of the Google Spreadsheet. Spreadsheet id can be found in the Spreadsheet URL between /d/ and /edit, eg:
                 https://docs.google.com/spreadsheets/d/<spreadsheetId>/edit#gid=0
+            client (googleapiclient.discovery.Resource): connection to the Google API Sheets resource.
         """
 
         from pygsuite import Clients
@@ -69,9 +105,8 @@ class Spreadsheet:
         # queues to add to and run in flush()
         self._spreadsheets_update_queue = []
         self._values_update_queue = []
-        # self._values_get_queue = []
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Union[str, int]):
 
         # if the key is an integer, use the int as worksheet index
         if isinstance(key, int):
@@ -87,37 +122,49 @@ class Spreadsheet:
                     )
                 )
         else:
-            raise ValueError("The key must be a string or integer. Use a string to get a worksheet by name, and integer to get a worksheet by index.")
+            raise ValueError(
+                "The key must be a string or integer. Use a string to get a worksheet by name, and integer to get a worksheet by index."
+            )
 
     @property
     def worksheets(self):
+        """List of Worksheet objects for each worksheet in the Spreadsheet.
+        """
         return [Worksheet(sheet, self) for sheet in self._spreadsheet.get("sheets")]
 
     def refresh(self):
+        """Method to refresh the spreadsheets API connection.
+        """
         self._spreadsheet = self.service.spreadsheets().get(spreadsheetId=self.id).execute()
 
     @retry((HttpError), tries=3, delay=10, backoff=5)
     def flush(
         self,
-        reverse=False,
-        value_input_option="RAW",
-        include_values_in_response=False,
-        response_value_render_option="FORMATTED_VALUE",
-        response_date_time_render_option="SERIAL_NUMBER",
+        reverse: bool = False,
+        value_input_option: ValueInputOption = ValueInputOption.RAW,
+        include_values_in_response: bool = False,
+        response_value_render_option: ValueRenderOption = ValueRenderOption.FORMATTED_VALUE,
+        response_date_time_render_option: DateTimeRenderOption = DateTimeRenderOption.SERIAL_NUMBER,
     ):
+        """Method to execute the change queues created through other methods.
 
-        assert value_input_option in self.VALUE_INPUT_OPTIONS
-        assert response_value_render_option in self.VALUE_RENDER_OPTIONS
-        assert response_date_time_render_option in self.DATE_TIME_RENDER_OPTIONS
+        Args:
+            reverse (bool): If True, the changes queued will be executed in reverse order.
+            value_input_option (ValueInputOption): Indicates which dimension an operation should apply to.
+            include_values_in_response (bool): If True, the API response will include the values changed.
+            response_value_render_option (ValueRenderOption): Determines how values should be rendered in the output.
+            response_date_time_render_option (DateTimeRenderOption): Determines how dates should be rendered in the output.
+
+        Returns:
+            response_dict (dict): Dictionary of API response JSON.
+        """
 
         if reverse:
             _spreadsheets_update_queue = reversed(self._spreadsheets_update_queue)
             _values_update_queue = reversed(self._values_update_queue)
-            # _values_get_queue = reversed(self._values_get_queue)
         else:
             _spreadsheets_update_queue = self._spreadsheets_update_queue
             _values_update_queue = self._values_update_queue
-            # _values_get_queue = self._values_get_queue
 
         response_dict = dict()
 
@@ -125,7 +172,7 @@ class Spreadsheet:
             response_dict["spreadsheets_update_response"] = (
                 self.service.spreadsheets()
                 .batchUpdate(body={"requests": _spreadsheets_update_queue}, spreadsheetId=self.id)
-                .execute()  # ["responses"]
+                .execute()["responses"]
             )
 
         if len(_values_update_queue) > 0:
@@ -135,19 +182,18 @@ class Spreadsheet:
                 .batchUpdate(
                     spreadsheetId=self.id,
                     body={
-                        "valueInputOption": value_input_option,
+                        "valueInputOption": value_input_option.value,
                         "data": _values_update_queue,
                         "includeValuesInResponse": include_values_in_response,
-                        "responseValueRenderOption": response_value_render_option,
-                        "responseDateTimeRenderOption": response_date_time_render_option,
+                        "responseValueRenderOption": response_value_render_option.value,
+                        "responseDateTimeRenderOption": response_date_time_render_option.value,
                     },
                 )
-                .execute()  # ["responses"]
+                .execute()["responses"]
             )
 
         self._spreadsheets_update_queue = []
         self._values_update_queue = []
-        # self._values_get_queue = []
         self.refresh()
 
         return response_dict
@@ -156,69 +202,101 @@ class Spreadsheet:
 
         pass
 
-    def get_values_from_ranges(
-        self, ranges,
+    def get_values_from_range(
+        self, cell_range: str,
     ):
+        """Method to get data from a Spreadsheet range.
+
+        Args:
+            cell_range (str): The range of cells for which to get data.
+
+        Returns:
+            self (Spreadsheet): The object itself, to method chain.
+        """
 
         get_response = (
             self.service.spreadsheets()
             .values()
-            .batchGet(spreadsheetId=self.id, ranges=ranges)
+            .batchGet(spreadsheetId=self.id, ranges=[cell_range])
             .execute()
         )
 
-        # TODO: because the request supports getting data from multiple ranges at once, this is a list
-        # however, the output of this is a litte bit messy at the moment--is a dict better or
-        # limiting this to read one valueRange at a time?
-        values_out = []
+        value_range = get_response.get("valueRanges")[0]  # TODO: cleanup
+        values = value_range.get("values")
 
-        for value_range in get_response.get("valueRanges"):
+        self._values = values
 
-            values = value_range.get("values")
-            values_out.append(values)
+        return self
 
-        return values_out
+    def to_list(self,):
+        """Method to use with self.get_values_from_range(self, cell_range) to return a list of values.
 
-    def get_data_from_ranges(self, ranges):
+        Returns:
+            self._values (list): A list of the cell data gotten from get_values_from_range()
+        """
 
-        # TODO: should this method support getting multiple ranges of data at once?
-        # The request allows for it, but this complicates the inputs / outputs of the method
+        assert len(self._values) > 0
 
-        get_response = (
-            self.service.spreadsheets()
-            .values()
-            .batchGet(spreadsheetId=self.id, ranges=ranges)
-            .execute()
-        )
+        return self._values
 
-        dfs = dict()
+    def to_df(
+        self, header: bool = True,
+    ):
+        """Method to use with self.get_values_from_range(self, cell_range) to return a pandas.DataFrame of values.
 
-        for value_range in get_response.get("valueRanges"):
+        Returns:
+            df (pd.DataFrame): A pandas.DataFrame of the cell data gotten from get_values_from_range()
+        """
 
-            # TODO: do we want to handle header rows here, or leave that to end users?
-            # if headers are optional, would the option be for each range of data being fetched?
+        assert len(self._values) > 0
 
-            values = value_range.get("values")
-            df = pd.DataFrame.from_records(data=values)
-            dfs[value_range.get("range")] = df
+        if header:
+            header = self._values[0]
+            records = self._values[1:]
+            df = pd.DataFrame.from_records(data=records, columns=header)
+        else:
+            records = self._values
+            df = pd.DataFrame.from_records(data=records)
 
-        return dfs
+        return df
 
     def insert_data(
-        self, insert_range, values, major_dimension="ROWS",
+        self, insert_range: str, values: list, major_dimension: Dimension = Dimension.ROWS,
     ):
+        """Method to insert data from a list into a given range in the Spreadsheet.
 
-        assert major_dimension in self.DIMENSIONS
+        Args:
+            insert_range (str): The cell range in which to insert data.
+            values (list): The data values, as a list, to insert.
+            major_dimension (Dimension): Indicates which dimension an operation should apply to.
 
-        value_range = {"range": insert_range, "majorDimension": major_dimension, "values": values}
+        Returns:
+            self (Spreadsheet): The object itself, to method chain.
+        """
+
+        value_range = {
+            "range": insert_range,
+            "majorDimension": major_dimension.value,
+            "values": values,
+        }
 
         self._values_update_queue.append(value_range)
 
         return self
 
     def insert_data_from_df(
-        self, df, insert_range, major_dimension="ROWS",
+        self, df: pd.DataFrame, insert_range: str, major_dimension: Dimension = Dimension.ROWS,
     ):
+        """Method to insert data from a pd.DataFrame into a given range in the Spreadsheet.
+
+        Args:
+            df (pd.DataFrame): Dataframe of data to insert.
+            insert_range (str): The cell range in which to insert data.
+            major_dimension (Dimension): Indicates which dimension an operation should apply to.
+
+        Returns:
+            self (Spreadsheet): The object itself, to method chain.
+        """
 
         # TODO: this insert_data_from_df method might be better as a method that we can
         # attach to insert_data--something like: mySpreadsheet.insert_data().from_df()
@@ -234,3 +312,5 @@ class Spreadsheet:
         self.insert_data(
             insert_range=insert_range, values=values, major_dimension=major_dimension,
         )
+
+        return self
