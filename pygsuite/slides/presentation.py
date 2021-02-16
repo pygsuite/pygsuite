41e453dca949d5a5a4c8fdf04fa32e910987c53c
Sam Gaudet <sgaudet1@babson.edu>
2021-02-16 12:11:00 -0500

    import HttpError

--- pygsuite/slides/presentation.py
+++ pygsuite/slides/presentation.py
@@ -60,7 +60,7 @@ class Presentation:
         self._mutation([{"slideObjectIds": [Slide.id], "insertionIndex": index}])
         self.flush()
 
-    @retry((HttpError), tries=3, delay=5, backoff=3)
+    @retry((HTTPError), tries=3, delay=5, backoff=3)
     def flush(self, reverse=False):
         if reverse:
             base = reversed(self._change_queue)
