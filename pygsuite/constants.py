bc2f032ba7affc9121ade1730e25ac26ff680b91
Sam Gaudet <sgaudet1@babson.edu>
2021-06-17 18:29:09 -0400

    update index_to_alphabet to handle bigger indexes

--- pygsuite/constants.py
+++ pygsuite/constants.py
@@ -1,9 +1,19 @@
 from os import environ, path
 
 
+# AUTH
 def get_credentials():
     dirname = path.dirname(path.dirname(__file__))
     return path.join(dirname, "credentials.json")
 
 
 CREDENTIALS = environ.get("GOOGLE_APPLICATION_CREDENTIALS") or get_credentials()
+
+# COMMON
+
+# SHEETS
+SHEETS_MAX_COLUMN_NUMBER = 18278
+
+# DOCS
+
+# SLIDES
