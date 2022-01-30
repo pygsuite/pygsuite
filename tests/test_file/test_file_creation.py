69c5b0fa1559fc6a59bb8fee7b45333b56d13d1f
Sam Gaudet <sgaudet1@babson.edu>
2022-01-30 12:27:57 -0500

    some file test coverage

---
+++ tests/test_file/test_file_creation.py
@@ -0,0 +1,81 @@
+from io import BytesIO
+import requests
+import uuid
+
+from pygsuite.drive import File
+from pygsuite.enums import GoogleDocFormat, GoogleMimeType
+
+
+TEST_ID = uuid.uuid4()
+
+
+def test_file_creation__empty_file(auth_test_clients):
+    new_file = File.create(name=f"Empty file {TEST_ID}")
+    assert new_file.id is not None
+    new_file.delete()
+
+
+def test_file_creation__starred_file(auth_test_clients):
+    new_file = File.create(name=f"Starred file {TEST_ID}", starred=True)
+    assert new_file.id is not None
+    new_file.delete()
+
+
+def test_file_creation__specified_mimetype(auth_test_clients):
+    new_file = File.create(name=f"Empty text file {TEST_ID}", mimetype="text/plain")
+    assert new_file.id is not None
+    new_file.delete()
+
+
+def test_file_creation__specified_google_mimetype(auth_test_clients):
+    new_file = File.create(name=f"Empty Spreadsheet {TEST_ID}", mimetype=GoogleMimeType.SHEETS)
+    assert new_file.id is not None
+    new_file.delete()
+
+
+def test_file_creation__from_bytes(auth_test_clients):
+    response = requests.get(
+        "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"
+    )
+    bytes = BytesIO(response.content)
+
+    new_file = File.create(
+        name=f"Test image file {TEST_ID}",
+        mimetype="image/svg+xml",
+        media_body=bytes,
+    )
+    assert new_file.id is not None
+    new_file.delete()
+
+
+def test_file_creation__custom_body(auth_test_clients):
+    custom_body = {"description": "minimal test of custom body"}
+    new_file = File.create(
+        name=f"Custom body test {TEST_ID}",
+        mimetype="text/plain",
+        extra_body=custom_body,
+    )
+    assert new_file.id is not None
+    new_file.delete()
+
+
+def test_file_upload__from_local_file(auth_test_clients):
+    upload_file = r".\assets\test.txt"
+    new_file = File.upload(
+        filepath=upload_file,
+        name=f"Test test upload {TEST_ID}",
+        mimetype="text/plain",
+    )
+    assert new_file.id is not None
+    new_file.delete()
+
+
+def test_file_upload__from_local_file__with_conversion(auth_test_clients):
+    upload_file = r".\assets\test data.xlsx"
+    new_file = File.upload(
+        filepath=upload_file,
+        name=f"Test Excel upload {TEST_ID}",
+        convert_to=GoogleDocFormat.SHEETS,
+    )
+    assert new_file.id is not None
+    new_file.delete()
