55d1069c8b6115f58f52dc8dbb14f6f612c918ce
Sam Gaudet <sgaudet1@babson.edu>
2022-01-31 19:51:30 -0500

    add more mimetypes

--- pygsuite/enums.py
+++ pygsuite/enums.py
@@ -18,12 +18,25 @@ class UserType(Enum):
 
 
 class MimeType(Enum):
+    UNKNOWN = ""
+    # Google MIME Types
     SHEETS = "application/vnd.google-apps.spreadsheet"
     DOCS = "application/vnd.google-apps.document"
     SLIDES = "application/vnd.google-apps.presentation"
     FOLDER = "application/vnd.google-apps.folder"
+    # Some other common MIME Types for convenience:
+    # Text files
+    PLAIN_TEXT = "text/plain"
+    HTML = "text/html"
     PDF = "application/pdf"
-    UNKNOWN = ""
+    # Microsoft Office files
+    MS_WORD = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    MS_EXCEL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    MS_POWERPOINT = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    # Image files
+    JPEG = "image/jpeg"
+    PNG = "image/png"
+    SVG = "image/svg+xml"
 
     def __str__(self):
         return self.value
