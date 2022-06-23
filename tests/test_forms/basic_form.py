470d109c17cc510711fa7776910989183136c5ae
Ethan Dickinson <ethan.dickinson@gmail.com>
2022-06-23 19:55:05 -0400

    Add tests that cover moving the same object.

--- tests/test_forms/basic_form.py
+++ tests/test_forms/basic_form.py
@@ -1,7 +1,9 @@
 from pygsuite.forms import Item, QuestionItem, Question, TextQuestion, Form
 import json
+import pytest
 
 
+@pytest.mark.run(order=1)
 def test_form(test_form):
     new_item = Item(
         question_item=QuestionItem(question=Question(text_question=TextQuestion(paragraph=True))),
@@ -9,7 +11,21 @@ def test_form(test_form):
         title="My fun question",
     )
     test_form.items.append(new_item)
+    test_form.items.append(new_item)
     test_form.flush()
+
+
+@pytest.mark.run(order=2)
+def test_auto_sync(test_form):
+    candidate = test_form.items[0]
+    cached = candidate.item_id
+    candidate.title = "At first"
+    test_form.items.move(0, 1)
+    test_title = "whose on second?"
+    candidate.title = test_title
+    test_form.flush()
+    assert test_form.items[1].item_id == cached
+    assert test_form.items[1].title == test_title
 
 
 def test_complex_parsing():
